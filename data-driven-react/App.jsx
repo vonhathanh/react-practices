8413aaccf7eee43d9b18e9614d19d85ecc994661
hanhvn <vonhathanh@gmail.com>
2025-01-07 10:53:54 +0700

    finish travel journal page

--- data-driven-react/App.jsx
+++ data-driven-react/App.jsx
@@ -1,34 +1,24 @@
 import Header from "./components/Header"
 import Entry from "./components/Entry"
-
-/**
- * Challenge: pass props to the Entry component to display
- * its data. See the `data.md` file for each prop name and its
- * value.
- * 
- * Then on the Entry component, receive and display the values
- * for those props. In the end, the page should look the same
- * as it does now, but without all the hard-coded data in the
- * component
- */
+import data from "./data"
 
 export default function App() {
-    const img = {
-        src: "https://scrimba.com/links/travel-journal-japan-image-url",
-        alt: "Mount Fuji"
-    }
+    const entries = data.map((entry) => {
+        return <Entry 
+            img={entry.img}
+            title={entry.title}
+            country={entry.country}
+            googleMapsLink={entry.googleMapsLink}
+            dates={entry.dates}
+            text={entry.text}
+        />
+    })
+
     return (
         <>
             <Header />
             <main className="container">
-                <Entry 
-                    img={img}
-                    title="Mount Fuji"
-                    country="Japan"
-                    googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                    dates="12 Jan, 2021 - 24 Jan, 2021"
-                    text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
-                />
+                {entries}
             </main>
         </>
     )
