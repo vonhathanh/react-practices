7d2bc7429fc601bbcb37e8fefa9001c12fa20852
hanhvn <vonhathanh@gmail.com>
2025-01-07 09:20:04 +0700

    implement contact page

---
+++ contact-page/Contact.jsx
@@ -0,0 +1,25 @@
+export default function Contact({img, name, phone, email}) {
+    return (
+        <article className="contact-card">
+            <img
+                src={img}
+                alt="Photo of Mr. Whiskerson"
+            />
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img
+                    src="./images/phone-icon.png"
+                    alt="phone icon"
+                />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img
+                    src="./images/mail-icon.png"
+                    alt="mail icon"
+                />
+                <p>{email}</p>
+            </div>
+        </article>
+    )
+}
