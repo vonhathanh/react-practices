39ba5da46b9af351a1046797ea83df22b09b04fe
hanhvn <vonhathanh@gmail.com>
2025-01-09 08:16:46 +0700

    add conditional rendering for jokes

--- jokes-page/Joke.jsx
+++ jokes-page/Joke.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react"
+
 export default function Joke({ setup, punchline }) {
+
+    const [isShown, setIsShown] = useState(false)
+
+    function onClick() {
+        setIsShown(oldValue => !oldValue)
+    }
+
     return (
         <>
-            {setup && <span>Setup: {setup}</span>}
-            <p>Punchline: {punchline}</p>
+            {setup && <h3>Setup: {setup}</h3>}
+            <p>{isShown && punchline}</p>
+            <button onClick={onClick}>Show punchline</button>
         </>
     )
 }
