ae7307ea02a8e2512394ac536bcf803a393aac76
hanhvn <vonhathanh@gmail.com>
2025-01-10 08:41:14 +0700

    add starwar page

---
+++ side-effects/fetch-starwar-data/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+export default function App(props) {
+    const [starWarsData, setStarWarsData] = React.useState(null)
+    
+    /**
+     * Challenge:
+     * Instead of console logging the data, save it in state
+     * and display it to the page. (Just replace the hard-coded
+     * object inside the `<pre>` element with the data)
+     */
+    
+    fetch("https://swapi.dev/api/people/1")
+        .then(res => res.json())
+        .then(data => {
+            console.log("data")
+            setStarWarsData(oldData => data)
+        })
+    
+    return (
+        <div>
+            <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
+        </div>
+    )
+}
--- side-effects/meme-generator/components/Main.jsx
+++ side-effects/meme-generator/components/Main.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 export default function Main() {
     /**
      * Challenge: move the hardcoded meme info into React
@@ -5,32 +7,48 @@ export default function Main() {
      * and `image` properties, and set the initial values to
      * the ones hardcoded below.
      */
-    
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="One does not simply"
-                        name="topText"
-                    />
-                </label>
 
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Walk into Mordor"
-                        name="bottomText"
-                    />
-                </label>
-                <button>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" />
-                <span className="top">One does not simply</span>
-                <span className="bottom">Walk into Mordor</span>
-            </div>
-        </main>
-    )
+    const [memeInfo, setMemeInfo] = useState({
+        imageUrl: "http://i.imgflip.com/1bij.jpg",
+        topText: "One does not simply",
+        bottomText: "Walk into Mordor"
+    })
+
+    function handleKeyPressed(e) {
+        const { value, name } = e.currentTarget
+        setMemeInfo(meme => ({
+            ...meme,
+            [name]: value
+        }))
+}
+
+return (
+    <main>
+        <div className="form">
+            <label>Top Text
+                <input
+                    type="text"
+                    placeholder="One does not simply"
+                    name="topText"
+                    onChange={handleKeyPressed}
+                />
+            </label>
+
+            <label>Bottom Text
+                <input
+                    type="text"
+                    placeholder="Walk into Mordor"
+                    name="bottomText"
+                    onChange={handleKeyPressed}
+                />
+            </label>
+            <button>Get a new meme image 🖼</button>
+        </div>
+        <div className="meme">
+            <img src={memeInfo.imageUrl} />
+            <span className="top">{memeInfo.topText}</span>
+            <span className="bottom">{memeInfo.bottomText}</span>
+        </div>
+    </main>
+)
 }
