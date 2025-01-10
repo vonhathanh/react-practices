0b92a45e6ce5c5076b8161daa6dea4013ae2e240
hanhvn <vonhathanh@gmail.com>
2025-01-10 11:08:33 +0700

    practice useEffect

--- side-effects/fetch-starwar-data/App.jsx
+++ side-effects/fetch-starwar-data/App.jsx
@@ -2,23 +2,28 @@ import React from "react"
 
 export default function App(props) {
     const [starWarsData, setStarWarsData] = React.useState(null)
-    
-    /**
-     * Challenge:
-     * Instead of console logging the data, save it in state
-     * and display it to the page. (Just replace the hard-coded
-     * object inside the `<pre>` element with the data)
-     */
-    
-    fetch("https://swapi.dev/api/people/1")
-        .then(res => res.json())
-        .then(data => {
-            console.log("data")
-            setStarWarsData(oldData => data)
-        })
-    
+    const [count, setCount] = React.useState(0)
+
+    React.useEffect(() => {
+        console.log("effect fn ran")
+        const url = `https://api.etherscan.io/v2/api?
+                        chainid=${count}
+                        &module=account
+                        &action=balance
+                        &address=0xb5d85cbf7cb3ee0d56b3bb207d5fc4b82f43f511
+                        &tag=latest&apikey=XCUKA5VFUBN9J39Q5DTAA15TY7PJSA5CUC`
+        console.log("url ", url)
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                setStarWarsData(data)
+            })
+    }, [count])
+
     return (
         <div>
+            <h2>The count is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
