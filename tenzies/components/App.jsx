c9a7033e19c9eeb521011d0c192afbc2114500cb
hanhvn <vonhathanh@gmail.com>
2025-01-13 15:52:27 +0700

    add 10 dicesto app

--- tenzies/components/App.jsx
+++ tenzies/components/App.jsx
@@ -1,20 +1,31 @@
+import { useState } from "react";
 import Dice from "./Dice"
 
+const NUM_DICES = 10;
+
+function getRandomInteger(a, b) {
+    return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+
+function generateAllNewDices() {
+    const dices = []
+    for (let i = 0; i < NUM_DICES; i++) {
+        dices.push(getRandomInteger(1, 6))
+    }
+    return dices
+}
+
 export default function App() {
+    const [dices, setDices] = useState(generateAllNewDices())
+    const diceElements = dices.map((dice, index) => <Dice value={dice} key={index} />)
     return (
         <main>
             <div className="game-board">
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
-                <Dice value={1} />
+                {diceElements}
             </div>
+
+            <button className="rollButton">Roll</button>
         </main>
     )
 }
--- tenzies/index.jsx
+++ tenzies/index.jsx
@@ -1,11 +1,20 @@
 import App from "./components/App"
 import ReactDOM from "react-dom/client"
 /**
- * Challenge: Start a brand new React app!
- * - Create a separate App component
- * - Import and render the App component here
- * - In the App component, render a <main> element
- * - Style everything to look like the slide
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ *      - Hints: Create a container to hold the 10 instances
+ *        of the Die component, and use CSS Grid to lay them
+ *        out evenly in 2 rows of 5 columns
+ *      - Use flexbox on main to center the dice container
+ *        in the center of the page
  */
+
 const root = document.getElementById("root")
 ReactDOM.createRoot(root).render(<App />)
