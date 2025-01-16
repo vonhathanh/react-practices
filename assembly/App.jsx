fb6f6e921bf903078775f402ec1f4c285fdb9bcb
hanhvn <vonhathanh@gmail.com>
2025-01-16 09:46:45 +0700

    add wrongGuessCount variable

--- assembly/App.jsx
+++ assembly/App.jsx
@@ -7,24 +7,26 @@ import KeyBoard from "./components/KeyBoard";
 import { languages } from "./languages";
 
 export default function Hangman() {
-  const alphabet = "abcdefghijklmnopqrstuvwxyz";
-  const [keyWord, setKeytWord] = useState("react");
-  const [guessedChars, setGuessedChars] = useState([]);
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const [keyWord, setKeytWord] = useState("react");
+    const [guessedChars, setGuessedChars] = useState([]);
 
-  function handleKeyboardClick(char) {
-    setGuessedChars((prevChars) =>
-      prevChars.includes(char) ? prevChars : [...prevChars, char]
+    const wrongGuessCount = guessedChars.reduce((sum, char) => keyWord.includes(char) ? sum += 0 : sum += 1, 0);
+
+    function handleKeyboardClick(char) {
+        setGuessedChars((prevChars) =>
+            prevChars.includes(char) ? prevChars : [...prevChars, char]
+        );
+    }
+
+    return (
+        <main>
+            <Header />
+            <Status />
+            <ProgrammingLanguages languages={languages} />
+            <KeyWord word={keyWord} guessedChars={guessedChars} />
+            <KeyBoard alphabet={alphabet} guessedChars={guessedChars} keyWord={keyWord} onClick={handleKeyboardClick} />
+            <button className="new-game">New Game</button>
+        </main>
     );
-  }
-
-  return (
-    <main>
-      <Header />
-      <Status />
-      <ProgrammingLanguages languages={languages} />
-      <KeyWord word={keyWord} />
-      <KeyBoard alphabet={alphabet} guessedChars={guessedChars} keyWord={keyWord} onClick={handleKeyboardClick} />
-      <button className="new-game">New Game</button>
-    </main>
-  );
 }
--- assembly/components/KeyWord.jsx
+++ assembly/components/KeyWord.jsx
@@ -1,6 +1,7 @@
 
-export default function KeyWord({word}) {
+export default function KeyWord({word, guessedChars}) {
     const chars = word.split("").map((c, i) => {
+        c = guessedChars.includes(c) ? c : ''
         return <div key={i}>{c}</div>
     })
 
